7cdcd1943a9966813a1deffcda4f7ce493d6d37c
Matteo Tammaccaro <m.tammaccaro@outlook.com>
2018-05-17 18:24:02 +0200

    ⚠ Aggiunta Property 'Skype' nelle saghe: 'Merp.Registry.CommandStack.Sagas.CompanySaga' 'Merp.Registry.CommandStack.Sagas.PersonSaga'

--- src/Merp.Registry.CommandStack/Sagas/PersonSaga.cs
+++ src/Merp.Registry.CommandStack/Sagas/PersonSaga.cs
@@ -51,7 +51,7 @@ namespace Merp.Registry.CommandStack.Sagas
             return Task.Factory.StartNew(() =>
             {
                 var country = string.IsNullOrWhiteSpace(message.Address) || !string.IsNullOrWhiteSpace(message.Country) ? message.Country : _defaultCountryResolver.GetDefaultCountry();
-                var person = Person.Factory.CreateNewEntry(message.FirstName, message.LastName, message.NationalIdentificationNumber, message.VatNumber, message.Address, message.City, message.PostalCode, message.Province, country, message.PhoneNumber, message.MobileNumber, message.FaxNumber, message.WebsiteAddress, message.EmailAddress, message.InstantMessaging);
+                var person = Person.Factory.CreateNewEntry(message.FirstName, message.LastName, message.NationalIdentificationNumber, message.VatNumber, message.Address, message.City, message.PostalCode, message.Province, country, message.PhoneNumber, message.MobileNumber, message.FaxNumber, message.WebsiteAddress, message.EmailAddress, message.InstantMessaging, message.Skype);
                 _repository.Save<Person>(person);
                 this.Data.PersonId = person.Id;
             });
@@ -62,7 +62,7 @@ namespace Merp.Registry.CommandStack.Sagas
             return Task.Factory.StartNew(() =>
             {
                 var country = string.IsNullOrWhiteSpace(message.Address) || !string.IsNullOrWhiteSpace(message.Country) ? message.Country : _defaultCountryResolver.GetDefaultCountry();
-                var person = Person.Factory.CreateNewEntryByImport(message.PersonId,message.RegistrationDate, message.FirstName, message.LastName, message.NationalIdentificationNumber, message.VatNumber, message.Address, message.City, message.PostalCode, message.Province, country, message.PhoneNumber, message.MobileNumber, message.FaxNumber, message.WebsiteAddress, message.EmailAddress, message.InstantMessaging);
+                var person = Person.Factory.CreateNewEntryByImport(message.PersonId,message.RegistrationDate, message.FirstName, message.LastName, message.NationalIdentificationNumber, message.VatNumber, message.Address, message.City, message.PostalCode, message.Province, country, message.PhoneNumber, message.MobileNumber, message.FaxNumber, message.WebsiteAddress, message.EmailAddress, message.InstantMessaging, message.Skype);
                 _repository.Save<Person>(person);
                 this.Data.PersonId = person.Id;
             });
