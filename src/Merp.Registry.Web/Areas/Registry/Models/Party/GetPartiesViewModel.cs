2b61b3fc007efa69e685d282a0c84b634628f06f
Alberto Mori <albi.mo87@hotmail.it>
2018-07-22 15:57:28 +0200

    add pagination to party list

--- src/Merp.Registry.Web/Areas/Registry/Models/Party/GetPartiesViewModel.cs
+++ src/Merp.Registry.Web/Areas/Registry/Models/Party/GetPartiesViewModel.cs
@@ -7,11 +7,22 @@ namespace Merp.Web.Site.Areas.Registry.Models.Party
 {
     public class GetPartiesViewModel
     {
-        public int id { get; set; }
+        public IEnumerable<PartyDescriptor> Parties { get; set; }
 
-        public Guid uid { get; set; }
+        public int TotalNumberOfParties { get; set; }
 
-        public string name { get; set; }
-        public string PhoneNumber { get; set; }
+        public class PartyDescriptor
+        {
+            public int id { get; set; }
+
+            public Guid uid { get; set; }
+
+            public string name { get; set; }
+            public string PhoneNumber { get; set; }
+
+            public string NationalIdentificationNumber { get; set; }
+
+            public string VatIndex { get; set; }
+        }
     }
 }
